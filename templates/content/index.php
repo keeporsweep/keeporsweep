1622eb4063d96954ca29e2813e9d4f21c969d722
Jan-Christoph Borchardt <hey@jancborchardt.net>
2018-03-27 13:38:51 +0200

    Add folder icon to detail row
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- templates/content/index.php
+++ templates/content/index.php
@@ -2,7 +2,7 @@
 	<div class="element-container">
 		<div class="element-preview icon-category-multimedia"></div>
 		<div class="element-name">{{ file.name }}</div>
-		<div class="element-detail">{{ file.path }}</div>
+		<div class="element-detail icon-folder">{{ file.path }}</div>
 	</div>
 	<div class="action-container">
 		<button v-on:click="remove" v-on:mouseover="pulse" class="action-remove icon-close-white">Nah</button>

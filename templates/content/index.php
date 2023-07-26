098efddc9a7df8217eef272b3c7f479168ed792d
Jan C. Borchardt <925062+jancborchardt@users.noreply.github.com>
2023-07-26 20:18:17 +0200

    Nicer subline and emptycontent text
    Signed-off-by: Jan C. Borchardt <925062+jancborchardt@users.noreply.github.com>

--- templates/content/index.php
+++ templates/content/index.php
@@ -2,27 +2,27 @@
 	<div class="element-container element-container-1 animated active">
 		<div class="element-preview"></div>
 		<div class="element-name">{{ file.name }}</div>
-		<div class="element-detail icon-folder">{{ file.path }}</div>
+		<div class="element-detail icon-folder">{{ file.path.substring(1, file.path.length - 1) }}</div>
 	</div>
 	<div class="element-container element-container-2 animated">
 		<div class="element-preview"></div>
 		<div class="element-name">{{ file.name }}</div>
-		<div class="element-detail icon-folder">{{ file.path }}</div>
+		<div class="element-detail icon-folder">{{ file.path.substring(1, file.path.length - 1) }}</div>
 	</div>
 	<div class="element-container element-container-3 animated">
 		<div class="element-preview"></div>
 		<div class="element-name">{{ file.name }}</div>
-		<div class="element-detail icon-folder">{{ file.path }}</div>
+		<div class="element-detail icon-folder">{{ file.path.substring(1, file.path.length - 1) }}</div>
 	</div>
 	<div class="element-container element-container-4 animated">
 		<div class="element-preview"></div>
 		<div class="element-name">{{ file.name }}</div>
-		<div class="element-detail icon-folder">{{ file.path }}</div>
+		<div class="element-detail icon-folder">{{ file.path.substring(1, file.path.length - 1) }}</div>
 	</div>
 	<div class="element-container element-container-empty">
 		<div class="element-empty icon-favorite"></div>
 		<div class="element-name">That’s all!</div>
-		<div class="element-detail">Refresh to go through your data again</div>
+		<div class="element-detail">Refresh to go again!</div>
 	</div>
 	<div class="action-container">
 		<button v-on:click="sweep"

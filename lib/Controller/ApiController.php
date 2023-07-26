e7e0a9d80097b7dc061874c18f86040a60a15cda
Jan C. Borchardt <925062+jancborchardt@users.noreply.github.com>
2023-07-26 18:19:29 +0200

    Fix code for newer Nextcloud versions, thanks to skjnldsv
    Signed-off-by: Jan C. Borchardt <925062+jancborchardt@users.noreply.github.com>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -3,9 +3,20 @@ namespace OCA\KeepOrSweep\Controller;
 
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\DataResponse;
+use OCP\Files\IRootFolder;
+use OCP\IRequest;
 use OC\Files\Filesystem;
 
 class ApiController extends Controller {
+
+	private IRootFolder $rootFolder;
+	private ?string $userId;
+
+	public function  __construct($appName, IRequest $request, IRootFolder $rootFolder, ?string $userId = null) {
+		parent::__construct($appName, $request);
+		$this->rootFolder = $rootFolder;
+		$this->userId = $userId;
+	}
 
 	/**
 	 * @NoAdminRequired
@@ -17,17 +28,18 @@ class ApiController extends Controller {
 			$path = $this->getRelativePath($files[$key]->getPath());
 			$results[$key]['path'] = $path;
 		}
-		
+
 		return new DataResponse($results);
 	}
 
 	private function getFilesRecursive(& $results = [], $path = '') {
-		$files = FileSystem::getDirectoryContent($path);
+		$userFolder = $this->rootFolder->getUserFolder($this->userId);
+		$files = $userFolder->get($path)->getDirectoryListing();
 		foreach($files as $file) {
 			if ($file->getType() === 'dir') {
 				$this->getFilesRecursive($results, $path . '/' . $file->getName());
 			} else {
-				$results[] = $file;
+				$results[] = $file->getFileInfo();
 			}
 		}
 
